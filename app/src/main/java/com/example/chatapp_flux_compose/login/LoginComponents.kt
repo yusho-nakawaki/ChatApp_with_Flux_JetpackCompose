69e890e369423bfefa9bd363e33c7e8ac8e9a9d2
yusho-nakawaki <ygoriko@gmail.com>
2022-03-30 23:30:10 +0900

    put compose in LoginComponents.kt

--- app/src/main/java/com/example/chatapp_flux_compose/login/LoginComponents.kt
+++ app/src/main/java/com/example/chatapp_flux_compose/login/LoginComponents.kt
@@ -1,2 +1,89 @@
 package com.example.chatapp_flux_compose.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.chatapp_flux_compose.R
+
+@Composable
+fun UserIcon(
+
+) {
+    Box (
+        modifier = Modifier.size(120.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        val imageBitMap = ImageBitmap.imageResource(R.drawable.ic_not_set_icon)
+        Image(
+            bitmap = imageBitMap,
+            contentDescription = "this image will be used in icon image",
+            modifier = Modifier
+                .border(
+                    width = 2.dp,
+                    color = Color.Gray,
+                    shape = RoundedCornerShape(60.dp)
+                )
+                .fillMaxSize()
+                .clip(CircleShape)
+                .clickable { /* Todo */ }
+        )
+        Image(
+            painter = painterResource(R.drawable.ic_plus_circle_black),
+            contentDescription = "add your icon image",
+            modifier = Modifier
+                .size(30.dp)
+                .clip(CircleShape)
+        )
+    }
+}
+
+@Composable
+fun userNameTextField(
+    userName: String,
+    onUserNameChanged: (String) -> Unit,
+) {
+    Row (
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Text(text = "ユーザー名:", fontSize = 15.sp)
+
+        TextField(
+            value = userName,
+            onValueChange = onUserNameChanged,
+            modifier = Modifier.size(200.dp, 50.dp)
+        )
+    }
+}
+
+@Composable
+fun CreateAccountButton (
+
+) {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .wrapContentHeight()
+            .width(200.dp)
+            .padding(10.dp)
+    ) {
+        Text(text = "Create new account")
+    }
+}
--- app/src/main/java/com/example/chatapp_flux_compose/login/LoginScreen.kt
+++ app/src/main/java/com/example/chatapp_flux_compose/login/LoginScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,60 +33,15 @@ fun LoginScreen(
         verticalArrangement = Arrangement.Center,
     ) {
 
-        Box (
-            modifier = Modifier.size(120.dp),
-            contentAlignment = Alignment.BottomEnd
-        ) {
-            val imageBitMap = ImageBitmap.imageResource(R.drawable.ic_not_set_icon)
-            Image(
-                bitmap = imageBitMap,
-                contentDescription = "this image will be used in icon image",
-                modifier = Modifier
-                    .border(
-                        width = 2.dp,
-                        color = Gray,
-                        shape = RoundedCornerShape(60.dp)
-                    )
-                    .fillMaxSize()
-                    .clip(CircleShape)
-                    .clickable { /* Todo */ }
-            )
-            Image(
-                painter = painterResource(R.drawable.ic_plus_circle_black),
-                contentDescription = "add your icon image",
-                modifier = Modifier
-                    .size(30.dp)
-                    .clip(CircleShape)
-            )
-        }
+        UserIcon()
 
         Spacer(modifier = Modifier.height(40.dp))
 
-        Row (
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-
-            Text(text = "ユーザー名:", fontSize = 15.sp)
-
-            val (userName, setUserName) = rememberSaveable { mutableStateOf("") }
-            TextField(
-                value = userName,
-                onValueChange = setUserName,
-                modifier = Modifier.size(200.dp, 50.dp)
-            )
-        }
+        val (userName, setUserName) = remember { mutableStateOf("") }
+        userNameTextField(userName, setUserName)
 
         Spacer(modifier = Modifier.height(60.dp))
 
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .wrapContentHeight()
-                .width(200.dp)
-                .padding(10.dp)
-        ) {
-            Text(text = "Create new account")
-        }
+        CreateAccountButton()
     }
 }
