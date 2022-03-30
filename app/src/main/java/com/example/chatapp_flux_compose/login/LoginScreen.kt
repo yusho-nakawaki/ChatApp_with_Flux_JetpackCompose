7ca8ef3e1f1c585dd8e6b47fb2a2a80afb496790
yusho-nakawaki <ygoriko@gmail.com>
2022-03-30 23:17:04 +0900

    install Button(Login button) in loginScreen

--- app/src/main/java/com/example/chatapp_flux_compose/login/LoginScreen.kt
+++ app/src/main/java/com/example/chatapp_flux_compose/login/LoginScreen.kt
@@ -1,14 +1,12 @@
 package com.example.chatapp_flux_compose.login
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -17,18 +15,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.chatapp_flux_compose.R
-import java.util.*
 
 @Composable
 fun LoginScreen(
@@ -64,23 +57,35 @@ fun LoginScreen(
                     .size(30.dp)
                     .clip(CircleShape)
             )
+        }
 
-            Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.height(40.dp))
 
-            Row (
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
+        Row (
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
 
-                Text(text = "ユーザー名:", fontSize = 15.sp)
+            Text(text = "ユーザー名:", fontSize = 15.sp)
 
-                val (userName, setUserName) = rememberSaveable { mutableStateOf("") }
-                TextField(
-                    value = userName,
-                    onValueChange = setUserName,
-                    modifier = Modifier.size(200.dp, 50.dp)
-                )
-            }
+            val (userName, setUserName) = rememberSaveable { mutableStateOf("") }
+            TextField(
+                value = userName,
+                onValueChange = setUserName,
+                modifier = Modifier.size(200.dp, 50.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(60.dp))
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .wrapContentHeight()
+                .width(200.dp)
+                .padding(10.dp)
+        ) {
+            Text(text = "Create new account")
         }
     }
 }
