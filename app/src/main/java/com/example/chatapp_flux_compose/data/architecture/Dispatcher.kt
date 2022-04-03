d0328834c480dc6124398756e9ef91d6b93a4f2c
yusho-nakawaki <ygoriko@gmail.com>
2022-04-03 20:58:02 +0900

    defined Dispatcher.kt

---
+++ app/src/main/java/com/example/chatapp_flux_compose/data/architecture/Dispatcher.kt
@@ -0,0 +1,32 @@
+package com.dena.mirrativ.architecture
+
+import android.util.Log
+import com.example.chatapp_flux_compose.BuildConfig
+import org.greenrobot.eventbus.EventBus
+
+class Dispatcher {
+    private val dispatcherBus: EventBus = EventBus.getDefault()
+
+    fun dispatch(event: Any) {
+        if (BuildConfig.DEBUG) {
+            Log.d("Dispatcher", event.toString())
+        }
+        dispatcherBus.post(event)
+    }
+
+    fun register(observer: Any) {
+        if (!dispatcherBus.isRegistered(observer)) {
+            dispatcherBus.register(observer)
+        } else {
+//            Logger.errorf("Subscriber $observer already registered to event ")
+        }
+    }
+
+    fun unregister(observer: Any) {
+        if (dispatcherBus.isRegistered(observer)) {
+            dispatcherBus.unregister(observer)
+        } else {
+//            Logger.errorf("Subscriber to unregister was not registered before: $observer")
+        }
+    }
+}
