ad26e1d5fb19dd047078ba19e6907f3a58f05db0
yusho-nakawaki <ygoriko@gmail.com>
2022-04-20 18:45:38 +0900

    keyboard will be closed when pushed done

--- app/src/main/java/com/example/chatapp_flux_compose/login/LoginComponents.kt
+++ app/src/main/java/com/example/chatapp_flux_compose/login/LoginComponents.kt
@@ -6,17 +6,22 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.chatapp_flux_compose.R
@@ -54,6 +59,7 @@ fun UserIcon(
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun UserNameTextField(
     userName: String,
@@ -66,10 +72,14 @@ fun UserNameTextField(
 
         Text(text = "ユーザー名:", fontSize = 15.sp)
 
+        val keyboardController = LocalSoftwareKeyboardController.current
         TextField(
             value = userName,
             onValueChange = onUserNameChanged,
-            modifier = Modifier.size(200.dp, 50.dp)
+            modifier = Modifier.size(200.dp, 50.dp),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = {keyboardController?.hide()})
         )
     }
 }
